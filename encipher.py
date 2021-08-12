f5e1d68de9c6e5f1d45c11fd985233063bd96228
georgegeiwitzcesana@gmail <georgegeiwitzcesana@gmail.com>
2021-08-12 13:05:47 -0700

    encipher

--- encipher.py
+++ encipher.py
@@ -17,7 +17,7 @@ print("output: " + to_filename)
 from_file = open(from_filename, 'r', encoding='utf8')
 to_file = open(to_filename, 'w', encoding='utf8')
 
-contents = from_file.read()
+contents = list(from_file.read())
 
 for index in range(len(contents)):
 
@@ -31,9 +31,8 @@ for index in range(len(contents)):
     cipher_letter = KEY[order]  
     contents[index] = cipher_letter
 
-to_file.write(contents)
+to_file.write(''.join(contents))
 
 from_file.close()
 to_file.close()
 
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               /                                                                                                                                                                                                                                                                                                                                                                                                                         
